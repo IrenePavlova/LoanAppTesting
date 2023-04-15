9ef13b48291ebbda79277bfba9a3e8eb46cfc60d
Irene Yacuk <irene.yacuk@gmail.com>
2023-04-15 14:52:32 -0600

    optimize imports

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,18 +1,10 @@
 import dataprovider.LoginDataProvider;
-import org.checkerframework.checker.units.qual.Temperature;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageobject.LoginPage;
-
-import java.util.concurrent.TimeUnit;
 
 public class Login {
     @Test(dataProvider = "Login", dataProviderClass = LoginDataProvider.class)
